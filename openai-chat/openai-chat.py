f1873c3d6287d7498be3d2cfb5197ce0e79b8bd9
Shlomit-B <shlomitbn@gmail.com>
2024-06-23 14:10:12 +0300

    add .env file management

--- openai-chat/openai-chat.py
+++ openai-chat/openai-chat.py
@@ -1,31 +1,44 @@
 from openai import OpenAI, OpenAIError
+from dotenv import load_dotenv
 import os
 import typer
 
 
 app = typer.Typer()
 
-@app.command()
-def question(question: str):
+def send_question(question: str):
     try:
-        client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+        load_dotenv()
+        key = os.environ["OPENAI_API_KEY"]
+        client = OpenAI(api_key=key)
         ans = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": question}])
-        print(ans.choices[0].message.content)
+        return ans
+    
+    except KeyError as e:
+        print('Please define the environment variable OPENAI_API_KEY in your .env file')
+        return None
+    
     except OpenAIError as e:
-        print(f'error: {e}')
+        print(f'Could not connect to get the answer from OpenAI:\n{e}')
+        return None    
 
 @app.command()
-def chat():
-    question = input('Message ChatGPT\n')
-    while (question != 'exit'):
-        client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-        ans = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": question}])
+def question(question: str):
+    ans = send_question(question)
+    if ans:
         print(ans.choices[0].message.content)
-        question = input()
+    
+@app.command()
+def chat():
+    inp = input('You can message ChatGPT now. When you finish, type "exit".\n')
+    while (inp != 'exit'):
+        ans = send_question(inp)
+        if not ans:
+            break
+        print(ans.choices[0].message.content)
+        inp = input()
 
 if __name__ == "__main__":
     app()
